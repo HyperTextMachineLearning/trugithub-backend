ca02cb8629f35c6cae544a088d3bb2780b5269fe
Ritik Matere <dev.ritik.matere@gmail.com>
2023-01-28 17:57:58 +0530

    Modify: Modified to send authenticated requests Solve: CORS issue and modified to return data as json string

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI, Request
 import requests
 import json
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+token = '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'
 
 @app.get("/profile/")
 async def get_profile(request: Request):
@@ -9,7 +24,13 @@ async def get_profile(request: Request):
     url = f"https://api.github.com/users/{params['username']}/repos"
 
     # repos is of Type Response[]
-    repos = requests.get(url=url)
+    repos = requests.get(url=url, headers={'Authorization': f'token {token}'})
+
+    # Submitted username contains invalid characters like space
+    if repos.status_code == 404:
+        return json.dumps({'message': 'Invalid User'})
+
+    _ = requests.get(url=f"https://api.github.com/users/{params['username']}", headers={'Authorization': f'token {token}'})
 
     # repos_list is of type dict[]
     repos_list = repos.json()
@@ -39,9 +60,8 @@ async def get_profile(request: Request):
             revised_repo[key] = repo[key]
 
         # Obtain languages->enlist->insert them
-        l = requests.get(url=repo['languages_url'])
+        l = requests.get(url=repo['languages_url'], headers={'Authorization': f'token {token}'})
         revised_repo['languages'] = list(l.json())
         
         revised_repos.append(revised_repo)
-
-    return revised_repos
+    return json.dumps(revised_repos)
