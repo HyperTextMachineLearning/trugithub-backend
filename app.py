38e69fe0260d668087f8bc00f9060565b0f0287b
Ritik Matere <dev.ritik.matere@gmail.com>
2023-01-31 19:10:46 +0530

    Add: User Details included; returned response is now Object
    Removed some useless keys from trimmed down response JSONs.
    Now returned reponse is Object as opposed to list

--- app.py
+++ app.py
@@ -16,47 +16,51 @@ app.add_middleware(
 )
 
 
-token = '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'
+token = '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'
 
 @app.get("/profile/")
 async def get_profile(request: Request):
     params = request.query_params
-    url = f"https://api.github.com/users/{params['username']}/repos"
+    repos_url = f"https://api.github.com/users/{params['username']}/repos?per_page=9"
 
     # repos is of Type Response[]
-    repos = requests.get(url=url, headers={'Authorization': f'token {token}'})
+    repos = requests.get(url=repos_url, headers={'Authorization': f'token {token}'})
 
-    # Submitted username contains invalid characters like space
+    # Submitted username not found or contains invalid characters like space
     if repos.status_code == 404:
         return json.dumps({'message': 'Invalid User'})
 
-    _ = requests.get(url=f"https://api.github.com/users/{params['username']}", headers={'Authorization': f'token {token}'})
+    user_details_url = f"https://api.github.com/users/{params['username']}"
+    user_details = requests.get(url=user_details_url, headers={'Authorization': f'token {token}'})
+    user_details = user_details.json() # Convert user_details from JSON to dict
+
+    required_details_keys = [
+        'avatar_url',
+        'name',
+        'location',
+        'bio',
+        'twitter_username',
+        'html_url',
+    ]
+
+    # Prepare trimmed down object for user details
+    final_user_details = {}
+    for key in required_details_keys:
+        final_user_details[key] = user_details[key]
+
 
     # repos_list is of type dict[]
     repos_list = repos.json()
-
-    required_keys = [
+    required_repo_keys = [
         'name',
-        'full_name',
         'html_url',
         'description',
     ]
-
-    # First element for revised_repos is the user's avatar URL
     revised_repos = []
-    avatar_extracted = False
     for repo in repos_list:
         revised_repo = {}
 
-        if not avatar_extracted:
-            revised_repos.append(
-                {
-                    'avatar_url': repo['owner']['avatar_url']
-                }
-            )
-            avatar_extracted = True
-
-        for key in required_keys:
+        for key in required_repo_keys:
             revised_repo[key] = repo[key]
 
         # Obtain languages->enlist->insert them
@@ -64,4 +68,9 @@ async def get_profile(request: Request):
         revised_repo['languages'] = list(l.json())
         
         revised_repos.append(revised_repo)
-    return json.dumps(revised_repos)
+    
+    response = { # response combines user details and all their repos
+        'user_details': final_user_details,
+        'repo_details': revised_repos
+    }
+    return json.dumps(response)
